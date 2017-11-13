c13470855d26c60e8b3932c3fb7a1fee009f0a94
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-11-13 10:17:19 +0100

    setting up firebase as test params

--- index.php
+++ index.php
@@ -1,18 +1,49 @@
-<?php
-// CATSS API
-include ("__factory/engine.php");
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CATSS API Sample Test</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/app.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
 
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="well">
+					<p>TOKEN:  6b971eac2f876685b4ff2d07ffeb545c41B756F2DCAC80BFD910D1BED0633974</p>
+				</div>
+				<br />
+				<div class="small">
+					<h4>Examplex</h4>
+					<p>
+						<b>EQUITY URL:</b> https://catss-api.herokuapp.com/?cq=pairs&token='token'
+					</p>
+					<p>
+						<b>ACCT BAL URL:</b> https://catss-api.herokuapp.com/?cq=acct_bal&token='token'
+					</p>
+					<p>
+						<b>STOCK BAL URL:</b> https://catss-api.herokuapp.com/?cq=stock_bal&token='token'
+					</p>
+					<p>
+						<b>TRANS INFO URL:</b> https://catss-api.herokuapp.com/?cq=trans_info&token='token'
+					</p>
+					<p>
+						<b>BASIC INFO URL:</b> https://catss-api.herokuapp.com/?cq=basic_info&token='token'
+					</p>
+					<p>
+						<b>NEWS URL:</b> https://catss-api.herokuapp.com/?cq=stock_news&token='token'
+					</p>
+					<p>
+						<b>INDEX NEWS URL:</b> https://catss-api.herokuapp.com/?cq=index_news&token='token'
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-if($_GET['cq'] && $_GET['token']){
-	// get check params
-	$token = $_GET['token'];
-	$type = $_GET['cq'];
-
-	$cats_engine = new CatssApi($token, $type);
-	$cats_engine->run();
-
-}else{
-	die('fail to send request !');
-}
-
-?>
+	
+</body>
+</html>
