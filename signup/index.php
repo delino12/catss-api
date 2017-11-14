49e426d493b4e77aaa8aef3316e061820c5cb746
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-11-14 11:47:55 +0100

    fixed Api bug

--- signup/index.php
+++ signup/index.php
@@ -1,15 +1,17 @@
 <?php
-// set request access
+// require engine 
 require ("../__factory/engine.php");
 
-// request data
-$token = $_REQUEST['token'];
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$password = $_REQUEST['password'];
+// decode json 
+$data = json_decode(file_get_contents('php://input'), true);
 
+// recieved users information from json
+$token = $data['token'];
+$name = $data['name'];
+$email = $data['email'];
+$password = $data['password'];
+
+// save users to database 
 $register_user = new SignupUser($token);
 $register_user->save($name, $email, $password);
 ?>
-
-
